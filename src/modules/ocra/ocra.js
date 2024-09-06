b78a87c696807170235a0ae83cdc9c5173499447
Jessé Azevêdo <jessedanobrega@gmail.com>
2024-09-06 16:41:34 -0300

    ocra generate

--- src/modules/ocra/ocra.js
+++ src/modules/ocra/ocra.js
@@ -1,1 +1,26 @@
+export class Ocra {
+  constructor(
+    key,
+    question,
+    suite,
+    len,
+    c = null,
+    ph = null,
+    s = null,
+    ts = null
+  ) {
+    this.key = key;
+    this.question = question;
+    this.suite = suite;
+    this.len = len;
+    this.c = c;
+    this.ph = ph;
+    this.s = s;
+    this.ts = ts;
+  }
 
+  async ocraGen() {
+    try {
+    } catch (error) {}
+  }
+}
